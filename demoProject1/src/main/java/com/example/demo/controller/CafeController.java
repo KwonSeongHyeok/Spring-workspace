d27a2d3fd9e18e4a339e4734862885f3dc6ec875
KwonSeongHyeok <aiqksiwk@naver.com>
2024-06-12 11:42:50 +0900

    cafe Controller index 생성

---
+++ demoProject1/src/main/java/com/example/demo/controller/CafeController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("cafe")
+@Slf4j
+public class CafeController {
+
+	@PostMapping("cafe_index")
+	public String cafeMainComment(@RequestParam("cafeCommentName") String cafeCommentName,
+								  @RequestParam("cafeCommentOpinion") String cafeCommentOpinion
+			) {
+		
+		log.info("카페 메인으로 이동하기");
+		log.debug("cafeCommentName : " + cafeCommentName);
+		log.debug("cafeCommentOpinion : " + cafeCommentOpinion);
+		
+		
+		return "redirect:/cafe/cafe_index";
+	}
+}
